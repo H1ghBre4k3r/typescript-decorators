c471e257cc92c1c5f50a8531c36cc564fcce08a6
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2021-12-26 15:38:20 +0100

    Add decorator for deprecation

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,3 +1,15 @@
 import { Deprecated } from "typescript-decorators";
 
-Deprecated();
+class TestClass {
+    private thing = 2;
+
+    @Deprecated
+    deprecatedFunction(): void {
+        // eslint-disable-next-line no-console
+        console.log(`thing: ${this.thing}`);
+    }
+}
+
+const t = new TestClass();
+t.deprecatedFunction();
+t.deprecatedFunction();
--- src/decorators/deprecated.ts
+++ src/decorators/deprecated.ts
@@ -1,3 +1,43 @@
-export function Deprecated() {
-    console.log("test");
+import { deprecate } from "util";
+
+type DeprecationMessage = string;
+
+function factory(message?: DeprecationMessage): MethodDecorator {
+    return (_target: unknown, propertyKey: string | symbol, descriptor: PropertyDescriptor) => {
+        const msg = message ?? `${String(propertyKey)} is deprecated!`;
+        // eslint-disable-next-line no-param-reassign
+        descriptor.value = deprecate(descriptor.value, msg);
+        return descriptor;
+    };
+}
+
+/**
+ * Mark a function as deprecated. This prints deprecation warnings to the console, when the deprecated function is called.
+ */
+export function Deprecated(
+    target: unknown,
+    propertyKey: string | symbol,
+    descriptor: PropertyDescriptor
+): PropertyDescriptor;
+
+/**
+ * Mark a function as deprecated with a custom deprecation message.
+ * @param message custom deprecation message
+ */
+export function Deprecated(message?: DeprecationMessage): MethodDecorator;
+
+export function Deprecated(
+    targetOrMessage?: DeprecationMessage | unknown,
+    propertyKey?: string | symbol,
+    descriptor?: PropertyDescriptor
+): PropertyDescriptor | MethodDecorator {
+    if (propertyKey && descriptor) {
+        const Decorator = factory();
+        const desc = Decorator(targetOrMessage as Record<string, unknown>, propertyKey, descriptor);
+        return desc as PropertyDescriptor;
+    }
+    if (typeof targetOrMessage === "string") {
+        return factory(targetOrMessage);
+    }
+    throw new Error("@Deprecated(msg) used with wrong parameter type for message!");
 }
