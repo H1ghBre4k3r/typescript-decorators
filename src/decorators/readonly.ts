16c24c76f19a6fe0a863028afaa7909f461ae184
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2021-12-26 21:50:55 +0100

    Add comments for readonly decorator

--- src/decorators/readonly.ts
+++ src/decorators/readonly.ts
@@ -1,7 +1,12 @@
+/**
+ * Make a property readonly. This means, that it can only be assigned once. Attempts to reassign this property will lead to a TypeError at runtime.
+ */
 export const Readonly: PropertyDecorator = (target, propertyKey) => {
+    // define property on target
     Object.defineProperty(target, propertyKey, {
         configurable: true,
         set(v: unknown) {
+            // in setter, redefine property, but this time non-writable
             Object.defineProperty(target, propertyKey, {
                 writable: false,
                 configurable: false,
